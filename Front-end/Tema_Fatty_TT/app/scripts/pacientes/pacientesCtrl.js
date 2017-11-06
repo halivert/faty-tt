cb12532002df56f270f8b878980e970961ec5e47
edgar.hurtado <edgar.hurtado@habilgroup.net>
2017-11-06 15:44:25 -0600

    Correcciones al script de pacientesCtrl y pacientesService

--- Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesCtrl.js
+++ Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesCtrl.js
@@ -3,15 +3,17 @@ angular.module('trabajoTerminal')
 .controller('pacientesCtrl', function($scope,$log,pacientesService,toastr,blockUI,$cookies){
   
    $scope.pacientes = [];
+   $scope.historialclinico = [];
+   $scope.usuario = "";
 
     $scope.verPacientes = function(){
-       $log.debug("ver pacientes");
+      $log.debug("ver pacientes");
 
-      var idMedico =  $cookies.get("idMedico")
+      var idUsuario =  $cookies.get("idUsuario")
 
-      $log.debug("idMedico : " + idMedico);
+      $log.debug("idUsuario : " + idUsuario);
 
-       pacientesService.recuperarListaPacientes(idMedico).then(
+       pacientesService.recuperarListaPacientes(idUsuario).then(
           function successCallback(d) {
             $log.debug("d" + JSON.stringify(d));
             
@@ -28,5 +30,31 @@ angular.module('trabajoTerminal')
           });
         
     }
+	
+	$scope.verHistorial = function(){
+		$log.debug("$scope.usuario" + $scope.usuario);
+		pacientesService.recuperarListaHistorialClinico($scope.usuario).then(
+          function successCallback(d) {
+            $log.debug("d" + JSON.stringify(d));
+           
+          },
+        
+          function errorCallback(d) {
+            if(d.data == null)
+              toastr.warning("Servicio no disponible", 'Advertencia');
+            else{
+              $log.debug("JSON.stringify(d.data.mensaje)" + JSON.stringify(d.data.mensaje));
+              toastr.error(d.data.mensaje, 'Error');
+            }
+          });
+		
+	}
+	
+	$scope.calcularEdad = function(nacimiento){
+		$log.debug("calcular Edar");
+		var ageDifMs = Date.now() - new Date(nacimiento);
+		var ageDate = new Date(ageDifMs); // miliseconds from epoch
+		return Math.abs(ageDate.getUTCFullYear() - 1970);
+	}
 
 });
--- Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesService.js
+++ Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesService.js
@@ -12,20 +12,86 @@ angular.module('trabajoTerminal')
             headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
           }
 
-      var url = 'http://35.188.191.232/tt-escom-diabetes/medico/pacientes/?idMedico='+idMedico;  
-      //var url = 'http://localhost:8080/tt-escom-diabetes/medico/pacientes/?idMedico='+idMedico;
+		  
+      var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/medico/'+idMedico+'pacientes/';  
+      //var url = 'http://localhost:8080/tt-escom-diabetes/ceres/medico/'+idMedico+'pacientes/;
 
       return $http.get(url,data,config).then(
 
         function successCallback(response) {
-          console.log("response OK : " + JSON.stringify(response.data));
-          return response.data;                   
+          
+		  if(response.data.respuesta === "OK"){
+			console.log("response OK : " + JSON.stringify(response.data));
+			return response.data;
+		  }
+		  else if (response.data.respuesta === "ERROR"){
+			console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
+			return $q.reject(response);
+		  }
+                             
         },
         function errorCallback(response) {
           console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
           return $q.reject(response);
         });
     }
+	
+	recuperarListaHistorialClinico: function(idPaciente) {
+      
+      var data = $.param({idPaciente: idPaciente});
+      var config = {
+            headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
+          }
+
+      var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/pacientes/'+idPaciente+'/historialclinico/';  
+      //var url = 'http://localhost:8080/tt-escom-diabetes/ceres/medico/'+idMedico+'pacientes/;
+
+      return $http.get(url,data,config).then(
+        function successCallback(response) {
+          
+		  if(response.data.respuesta === "OK"){
+			console.log("response OK : " + JSON.stringify(response.data));
+			return response.data;
+		  }
+		  else if (response.data.respuesta === "ERROR"){
+			console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
+			return $q.reject(response);
+		  }                   
+        },
+        function errorCallback(response) {
+          console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
+          return $q.reject(response);
+        });
+    }
+	
+	recuperarDetallHistorialClinico: function(idPaciente) {
+      
+      var data = $.param({idPaciente: idPaciente});
+      var config = {
+            headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
+          }
+
+      var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/pacientes/'+idPaciente+'/historialclinico/1';  
+      //var url = 'http://localhost:8080/tt-escom-diabetes/ceres/medico/'+idMedico+'pacientes/;
+
+      return $http.get(url,data,config).then(
+        function successCallback(response) {
+          
+		  if(response.data.respuesta === "OK"){
+			console.log("response OK : " + JSON.stringify(response.data));
+			return response.data;
+		  }
+		  else if (response.data.respuesta === "ERROR"){
+			console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
+			return $q.reject(response);
+		  }                   
+        },
+        function errorCallback(response) {
+          console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
+          return $q.reject(response);
+        });
+    }
+	
   }
 
 })
