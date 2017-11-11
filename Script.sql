73b1662cf2a9e8cd10355632a134bba311e612c2
Hali Velazquez <halivelazquez@outlook.com>
2017-11-11 15:02:37 -0600

    Best script

--- Script.sql
+++ Script.sql
@@ -4,7 +4,7 @@ CREATE TABLE USUARIO (
   APELLIDO_PATERNO VARCHAR(50) NOT NULL,
   APELLIDO_MATERNO VARCHAR(50) NOT NULL,
   EMAIL VARCHAR(50) NOT NULL UNIQUE,
-  KEYWORD BLOB NOT NULL,
+  KEYWORD VARCHAR(100) NOT NULL,
   FECHA_NACIMIENTO DATE NOT NULL,
   SEXO VARCHAR(10) NOT NULL
 );
@@ -78,3 +78,101 @@ CREATE TABLE ALIMENTOS (
   POTASIO DOUBLE(18,2) NOT NULL,
   AZUCAR_GR DOUBLE(18,2) NOT NULL
 );
+
+DELIMITER //
+CREATE PROCEDURE SP_MEDICO_ALTA (
+  IN _nombre VARCHAR(50),
+  IN _apellidoPaterno VARCHAR(50),
+  IN _apellidoMaterno VARCHAR(50),
+  IN _email VARCHAR(50),
+  IN _keyword VARCHAR(100),
+  IN _fechaNacimiento DATE,
+  IN _sexo VARCHAR(10),
+  IN _cedulaProfesional VARCHAR(20)
+)
+BEGIN
+  DECLARE vSalt VARCHAR(10);
+  DECLARE vKeyConv VARCHAR(100);
+  DECLARE vDigPass VARCHAR(100);
+  DECLARE vKeyTr VARCHAR(100);
+  DECLARE vI INT;
+  DECLARE nvoId INT;
+
+  SELECT COALESCE(MAX(ID_USUARIO), 1) INTO nvoId
+  FROM USUARIO;
+
+  SELECT SUBSTRING(MD5(RAND()) FROM 1 FOR 8) INTO vSalt;
+  SET vKeyConv = CONVERT(_keyword, BINARY);
+
+  SET vKeyTr = CONCAT(vKeyConv, vSalt);
+
+  SET vI = 0;
+  REPEAT
+    SET vKeyTr = MD5(vKeyTr);
+    SET vI = vI + 1;
+  UNTIL vI > 2330 END REPEAT;
+
+  SET vDigPass = TO_BASE64(CONCAT(vSalt, vKeyTr));
+  
+  INSERT INTO USUARIO
+  VALUES(
+    nvoId,
+    _nombre,
+    _apellidoPaterno,
+    _apellidoMaterno,
+    _email,
+    vDigPass,
+    _fechaNacimiento,
+    _sexo
+  );
+
+  INSERT INTO MEDICO
+  VALUES(
+    nvoId,
+    _cedulaProfesional
+  );
+
+END //
+
+CREATE PROCEDURE SP_LOGIN_USUARIO (
+  IN _email VARCHAR(100),
+  IN _keyword VARCHAR(100),
+  OUT _correcto BOOLEAN
+)
+BEGIN
+  DECLARE vSalt VARCHAR(10);
+  DECLARE vKeyConv VARCHAR(100);
+  DECLARE vDigPass VARCHAR(100);
+  DECLARE vKeyTr VARCHAR(100);
+  DECLARE vStKey VARCHAR(100);
+  DECLARE vI INT;
+
+  SELECT SUBSTRING(FROM_BASE64(KEYWORD) FROM 1 FOR 8) INTO vSalt
+  FROM USUARIO
+  WHERE EMAIL = _email;
+
+  SET vKeyConv = CONVERT(_keyword, BINARY);
+  SET vKeyTr = CONCAT(vKeyConv, vSalt);
+
+  SET vI = 0;
+  REPEAT
+    SET vKeyTr = MD5(vKeyTr);
+    SET vI = vI + 1;
+  UNTIL vI > 2330 END REPEAT;
+
+  SET vDigPass = TO_BASE64(CONCAT(vSalt, vKeyTr));
+
+  SELECT KEYWORD INTO vStKey
+  FROM USUARIO
+  WHERE EMAIL = _email;
+
+  IF STRCMP(vStKey, vDigPass) = 0 THEN
+    SELECT ID_USUARIO INTO _correcto
+    FROM USUARIO
+    WHERE EMAIL = _email;
+  ELSE
+    SET _correcto = -1;
+  END IF;
+  
+END //
+DELIMITER ;
