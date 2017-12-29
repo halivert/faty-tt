6d0c0686910289c5df54ff7c0fb94eda0020683c
edgar.hurtado <edgar.hurtado@habilgroup.net>
2017-12-29 17:02:51 -0600

    Se agrega el controlador para acutalizar historial clinico de paciente

--- Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesCtrl.js
+++ Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesCtrl.js
@@ -2,10 +2,11 @@
 * pacientesCtrl - Controlador ligado a la vista pacientes.html
 */
 angular.module('trabajoTerminal')
-.controller('pacientesCtrl', function($scope,$log,pacientesService,toastr,blockUI,$cookies,$filter,$state){
+.controller('pacientesCtrl', function($scope,$log,pacientesService,toastr,blockUI,$cookies,$filter,$state,$cookies){
 
   $scope.pacientes = [];
-  $scope.historialclinico = [];
+  $scope.listhistorialclinico = [];
+  //$scope.historial = {};
   $scope.usuario = {
     id:''
   };
@@ -14,20 +15,20 @@ angular.module('trabajoTerminal')
 /**
 * verPacientes  - Funcion que se ejecuta cuando se carga la vista de pacientes.html, lista los pacientes asociados a un medico
 */
-  $scope.verPacientes = function(){
-    $log.debug("ver pacientes");
+$scope.verPacientes = function(){
+  $log.debug("ver pacientes");
 
-    var idUsuario =  $cookies.get("idUsuario")
-    pacientesService.recuperarListaPacientes(idUsuario).then(
-      function successCallback(d) {
-        $scope.pacientes=d;
+  var idUsuario =  $cookies.get("idUsuario")
+  pacientesService.recuperarListaPacientes(idUsuario).then(
+    function successCallback(d) {
+      $scope.pacientes=d;
 
         //Integracion de los elementos recuperados por Angular con el plugin Datatable de JQuery
         angular.element(document).ready(function() {  
           dTable = $('#dataTable-pacientes')  
           dTable.DataTable({"language": {
-                "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-            }});  
+            "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+          }});  
         });  
       },
       function errorCallback(d){
@@ -37,42 +38,73 @@ angular.module('trabajoTerminal')
           toastr.error(d.data.mensaje, 'Error');
         }
       });
-  },
+},
 
 /**
 * recuperaPaciente  - Seleccionar la infor de un paciente un paciente
 */
-  $scope.recuperaPaciente = function(){
-    console.log("$scope.usuario.id : " + $scope.usuario.id);
-    $scope.currentPaciente = $filter('filter')($scope.pacientes, {'id_USUARIO':$scope.usuario.id});
-    console.log("$scope.currentPaciente : " + JSON.stringify($scope.currentPaciente) );
-  },
+$scope.recuperaPaciente = function(){
+  console.log("$scope.usuario.id : " + $scope.usuario.id);
+  $scope.currentPaciente = $filter('filter')($scope.pacientes, {'id_USUARIO':$scope.usuario.id});
+  console.log("$scope.currentPaciente : " + JSON.stringify($scope.currentPaciente) );
+},
 
 /**
 * verHistorial  - Recuperar la informacion del historial clinico del paciente seleccionado
 */
-  $scope.verHistorial = function(){
-    $log.debug("$scope.usuario" + $scope.usuario.id);
-    pacientesService.recuperarListaHistorialClinico($scope.usuario.id).then(
-      function successCallback(d) {
-        $state.transitionTo('index.historialClinico');
-        if(d.length == 0){
-          toastr.warning("El paciente aún no tiene historial clínico.", 'Atención');
-        }
-        dTable = $('#dataTable-historial-clinico')  
-          dTable.DataTable({"language": {
-              "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-          }});  
-      },
-      function errorCallback(d) {
-        if(d.data == null)
-          toastr.warning("Servicio no disponible", 'Advertencia');
-        else{
-          $log.debug("JSON.stringify(d.data.mensaje)" + JSON.stringify(d.data.mensaje));
-          toastr.error(d.data.mensaje, 'Error');
-        }
-      });
-  },
+$scope.verHistorial = function(){
+  $cookies.put("idCurrentPaciente",$scope.usuario.id);
+  $log.debug("$scope.usuario" + $scope.usuario.id);
+  pacientesService.recuperarListaHistorialClinico($cookies.get("idCurrentPaciente")).then(
+    function successCallback(d) {
+
+      $state.transitionTo('index.historialClinico');
+      if(d.length == 0){
+        toastr.warning("El paciente aún no tiene historial clínico.", 'Atención');
+      }else{
+        $scope.listhistorialclinico = d;
+        $log.debug("JSON.stringify(d)" + JSON.stringify(d));
+      }
+      dTable = $('#dataTable-historial-clinico')  
+      dTable.DataTable({"language": {
+        "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+      }});  
+    },
+    function errorCallback(d) {
+      if(d.data == null)
+        toastr.warning("Servicio no disponible", 'Advertencia');
+      else{
+        $log.debug("JSON.stringify(d.data.mensaje)" + JSON.stringify(d.data.mensaje));
+        toastr.error(d.data.mensaje, 'Error');
+      }
+    });
+},
+
+/**
+* guardarHistorialClinico  - Guardar la informacion del historial clinico del paciente seleccionado
+*/
+$scope.guardarHistorialClinico = function(){
+
+  console.log("guardarHistorialClinico()");
+  var date = new Date();
+    console.log("date : " + date);
+
+  pacientesService.guardarInfoHistorialClinico($scope.usuario.id,$scope.historial.peso,$scope.historial.talla,$scope.historial.estatura,$scope.historial.imc,$scope.historial.lipidos,$scope.historial.carbohidratos,$scope.historial.proteinas,$scope.historial.azucar).then(
+    
+    function successCallback(d) {
+      angular.element('#modal-paciente').modal('hide');
+      toastr.success(d, 'Ok');
+
+    },
+    function errorCallback(d) {
+      if(d.data == null)
+        toastr.warning("Servicio no disponible", 'Advertencia');
+      else{
+        $log.debug("JSON.stringify(d.data.mensaje)" + JSON.stringify(d.data.mensaje));
+        toastr.error(d.data.mensaje, 'Error');
+      }
+    });
+},  
 
 /**
 * calculateAge  - Calcular la edad de los pacientes con base en su fecha de nacimiento
--- Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesService.js
+++ Front-end/Tema_Fatty_TT/app/scripts/pacientes/pacientesService.js
@@ -1,20 +1,20 @@
 //Pacientes Service
 angular.module('trabajoTerminal')
 
-.service("pacientesService", function($log, $http, $q){
+.service("pacientesService", function($log, $http, $q, $filter){
 
   return {
 
 /**
 * recuperarListaPacientes - Funcion que se comunica con la API para recuperar la lista de pacientes asociados a un medico
 */
-    recuperarListaPacientes: function(idMedico) {
-      var data = $.param({idMedico: idMedico});
-      var config = {
-        headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
-      }
+recuperarListaPacientes: function(idMedico) {
+  var data = $.param({idMedico: idMedico});
+  var config = {
+    headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
+  }
 
-      var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/medico/'+idMedico+'/pacientes/';
+  var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/medico/'+idMedico+'/pacientes/';
       //var url = 'http://localhost:8080/tt-escom-diabetes/ceres/medico/'+idMedico+'/pacientes/';
 
       return $http.get(url,data,config).then(
@@ -35,13 +35,13 @@ angular.module('trabajoTerminal')
 /**
 * recuperarListaHistorialClinico  - Funcion que se comunica con la API para recuperar la lista de historial clinico de un paciente con base en su identficador
 */
-    recuperarListaHistorialClinico: function(idPaciente) {
-      var data = $.param({idPaciente: idPaciente});
-      var config = {
-        headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
-      }
+recuperarListaHistorialClinico: function(idPaciente) {
+  var data = $.param({idPaciente: idPaciente});
+  var config = {
+    headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
+  }
 
-      var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/pacientes/'+idPaciente+'/historialclinico/';  
+  var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/pacientes/'+idPaciente+'/historialclinico/';  
       //var url = 'http://localhost:8080/tt-escom-diabetes/ceres/pacientes/'+idPaciente+'/historialclinico/';
       return $http.get(url,data,config).then(
         function successCallback(response) {
@@ -60,16 +60,65 @@ angular.module('trabajoTerminal')
     },
 
 /**
-* recuperarDetallHistorialClinico - Recuperar la informacion detallada de un historial clinico 
+* guardarInfoHistorialClinico  - Funcion que se comunica con la API para guardar informarcion del historial clinico de un paciente
 */
-    recuperarDetallHistorialClinico: function(idPaciente) {
+guardarInfoHistorialClinico: function(idPaciente,peso,talla,estatura,imc,lipidos,carbohidratos,proteinas,azucar) {
 
-      var data = $.param({idPaciente: idPaciente});
-      var config = {
-        headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
+  
+  var fechaRegistro = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
+
+
+  var data = {
+    idPaciente: idPaciente,
+    fecha: fechaRegistro,
+    peso: peso,
+    talla: talla,
+    estatura: estatura,
+    imc: imc,
+    lipidos: lipidos,
+    carbohidratos: carbohidratos,
+    proteinas:proteinas,
+    azucar: azucar
+  };
+
+  
+
+  var config = {
+    headers : {'Content-Type': 'application/json'}
+  }
+
+  var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/pacientes/historialclinico';  
+
+  console.log("data : " + JSON.stringify(data));
+
+  return $http.post(url,data,config).then(
+    function successCallback(response) {
+      if(response.data.respuesta === "OK"){
+        console.log("response OK : " + JSON.stringify(response.data.mensaje));
+        return response.data.mensaje;     
+      }
+      else{
+        console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
+        return $q.reject(response);
       }
 
-      var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/pacientes/'+idPaciente+'/historialclinico/1';  
+    },function errorCallback(response) {
+      console.log("response ERROR : " + JSON.stringify(response.data.mensaje));
+      return $q.reject(response);
+    });
+},    
+
+/**
+* recuperarDetallHistorialClinico - Recuperar la informacion detallada de un historial clinico 
+*/
+recuperarDetallHistorialClinico: function(idPaciente) {
+
+  var data = $.param({idPaciente: idPaciente});
+  var config = {
+    headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
+  }
+
+  var url = 'http://35.188.191.232/tt-escom-diabetes/ceres/pacientes/'+idPaciente+'/historialclinico/1';  
       //var url = 'http://localhost:8080/tt-escom-diabetes/ceres/medico/'+idMedico+'pacientes/;
 
       return $http.get(url,data,config).then(
