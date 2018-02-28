b6cda971373aff7ffdd5c93793bf6d625faa4957
Hali Velazquez <halivelazquez@outlook.com>
2018-02-27 23:11:38 -0600

    Gitignore and stuff  Cambios a ser confirmados: 	modificado:     .gitignore 	borrado:        CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/FuncionesPrincipales.java 	renombrado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/FragmentoInicio.java -> CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/InicioFragment.java 	renombrado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/FragmentoLogin.java -> CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/LoginFragment.java 	renombrado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/FragmentoMedicoInicio.java -> CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/MedicoInicioFragment.java 	renombrado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/ActividadMedicoPrincipal.java -> CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/MedicoPrincipalActivity.java 	nuevo archivo:  CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/RegistrarGlucosaFragment.java 	nuevo archivo:  CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/TimePickerFragment.java 	nuevo archivo:  CeresMobile/app/src/main/res/layout/fragment_registrar_glucosa.xml
     Cambios no rastreados para el commit:
    	modificado:     CeresMobile/app/src/main/AndroidManifest.xml
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/ActividadPacientePrincipal.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/ActividadPrincipal.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/ActividadRegistro.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/DatePickerFragment.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/FragmentoPacienteInicio.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/InicioFragment.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/LoginFragment.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/MedicoInicioFragment.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/MedicoPrincipalActivity.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/PostJSONReq.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/PostReq.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/RegistrarGlucosaFragment.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/TimePickerFragment.java
    	modificado:     CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/Vars.java
    	modificado:     CeresMobile/app/src/main/res/layout/actividad_principal_medico.xml
    	modificado:     CeresMobile/app/src/main/res/layout/actividad_principal_paciente.xml
    	modificado:     CeresMobile/app/src/main/res/layout/fragment_inicio.xml
    	modificado:     CeresMobile/app/src/main/res/layout/fragment_login.xml
    	modificado:     CeresMobile/app/src/main/res/layout/fragment_medico_inicio.xml
    	modificado:     CeresMobile/app/src/main/res/layout/fragment_registrar_glucosa.xml
    	modificado:     CeresMobile/app/src/main/res/values/strings.xml
    
     Archivos sin seguimiento:
    	CeresMobile/app/src/main/res/drawable-nodpi/fa-edit.png~
    	CeresMobile/app/src/main/res/drawable-nodpi/fa_edit.png

---
+++ CeresMobile/app/src/main/java/escom/tt/ceres/ceresmobile/TimePickerFragment.java
@@ -0,0 +1,10 @@
+package escom.tt.ceres.ceresmobile;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by hali on 27/02/18.
+ */
+
+public class TimePickerFragment extends DialogFragment {
+}
